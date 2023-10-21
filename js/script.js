a2ac91f254c254796d16a5d94e4a413a3ebada64
Stjepan <stipe.milk@gmail.com>
2023-10-21 20:04:38 +0200

    implement smooth scrolling and close mobile navigation menu after a click on a link

--- js/script.js
+++ js/script.js
@@ -17,3 +17,34 @@ yearEl.textContent = currentYear;
 btnNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+/////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
