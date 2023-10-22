96682b7f5441555f6cf1da1689a8b94bcf8e8085
Stjepan <stipe.milk@gmail.com>
2023-10-22 14:21:45 +0200

    implement a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -48,3 +48,23 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+/////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
